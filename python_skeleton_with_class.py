a62224409d37236646d611add23e4c14d37f2dbc
Sean-Paul Bobadilla <seanpaul.bobadilla@gmail.com>
2024-08-12 17:42:50 -0700

    add a skeleton python script with class

---
+++ python_skeleton_with_class.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import inspect
+
+class myClass:
+  def __init__(self, myVar1):
+    self.myVars = myVar1
+                   
+  def __str__(self):
+    return f"{self.myVars}"
+    
+  def myFunc(self, newVal):
+      try:
+        # do something
+        if len(newVal) > 1:
+          self.myVars = newVal
+        else:
+          raise Exception("Something bad happened")
+        
+      except Exception as e:
+        print("error message ",e)
+        print("at: ",inspect.stack()[0][3]+"("+newVal+")")
+
+c1 = myClass("something")
+c1.myFunc("something new")
+
+print(str(c1))
+
+
+
+
