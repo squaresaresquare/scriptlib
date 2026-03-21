332eca6ffca8570eac4dc2f91f4b3e24ead639e6
squaresaresquare <126361574+squaresaresquare@users.noreply.github.com>
2026-03-21 16:05:39 -0700

    Update python_skeleton_with_class.py

--- skeletons/python_skeleton_with_class.py
+++ skeletons/python_skeleton_with_class.py
@@ -5,44 +5,44 @@ import signal
 import sys
 
 class myClass:
-  def __init__(self, myVar1):
-    self.myVars = myVar1
-    self.logger = logging.getLogger(__name__)
+    def __init__(self, myVar1):
+        self.myVars = myVar1
+        self.logger = logging.getLogger(__name__)
 
   #Some built in functions you can implement
   def __str__(self):
-    return f"{self.myVars}"
+        return f"{self.myVars}"
   def __repr__(self):
-    return f'Verbose {self.__name__}'
+        return f'Verbose {self.__name__}'
   def __dir__(self):
-    return ['myVars']
+        return ['myVars']
     
   #class functions
   def myFunc(self, newVal):
       try:
-        # do something
-        if len(newVal) > 1:
-          self.myVars = newVal
-        else:
-          raise Exception("Something bad happened")        
-      except Exception as e:
-        print("error message ",e)
-        print("at: ",inspect.stack()[0][3]+"("+newVal+")")
+          # do something
+          if len(newVal) > 1:
+              self.myVars = newVal
+          else:
+              raise Exception("Something bad happened")        
+        except Exception as e:
+            print("error message ",e)
+            print("at: ",inspect.stack()[0][3]+"("+newVal+")")
 
   # for iterable objects
   def __iter__(self):
-    if isinstance(myVars, list):
-      return self
-    else:
-      raise TypeError(255,'object is not a list') from error
+      if isinstance(myVars, list):
+          return self
+      else:
+          raise TypeError(255,'object is not a list') from error
     
   def __next__(self, MyVars):
-    if isinstance(self.myVars, list):
-      for elem in self.myVars:
-        yield elem
-      raise StopIteration
-    else:
-      raise TypeError(255,'object is not a list') from error
+      if isinstance(self.myVars, list):
+          for elem in self.myVars:
+              yield elem
+          raise StopIteration
+      else:
+          raise TypeError(255,'object is not a list') from error
       
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
